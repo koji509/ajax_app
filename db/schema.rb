e32da7aa4c7e2ac63fdaad755d9f4cd643ccc105
koji509 <thislifek@yahoo.co.jp>
2020-11-17 20:30:32 +0900

    AjaxAppの準備

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,8 @@
 ActiveRecord::Schema.define(version: 2020_10_27_034610) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.text "content"
+    t.text "memo"
+    t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
